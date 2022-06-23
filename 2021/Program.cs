bd515e8fd09e5095fb36e5e0216de3a67ba9ca78
Corbin King <9155069+cbking-@users.noreply.github.com>
2022-06-22 23:23:33 -0400

    2021 - Day 7

--- 2021/Program.cs
+++ 2021/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Diagnostics;
 using System.Reflection;
 using System.Text;
-using Newtonsoft.Json;
+using MathNet.Numerics.Statistics;
 using static Core.Helpers;
 
 var data = await LoadDataAsync(args[0]);
@@ -385,6 +385,37 @@ public static class AdventOfCode
         }
 
         Console.WriteLine($"Part 2: \x1b[93m{fishes.Select(kvp => kvp.Value).Sum()}\x1b[0m");
+        watch.Stop();
+        Console.WriteLine($"Part 2 Execution Time: {watch.ElapsedMilliseconds} ms");
+    }
+
+    public static void Day7(string[] data)
+    {
+        var watch = new System.Diagnostics.Stopwatch();
+        watch.Start();
+
+        var positions = Array.ConvertAll(data[0].Split(',', StringSplitOptions.RemoveEmptyEntries), double.Parse);
+        
+        //added a whole package for getting the median but their implementation is going to be way better
+        // than anything I would implement (copy + paste from stackoverflow)
+        var positionToMoveTo = positions.Median();
+
+        Console.WriteLine($"Part 1: \x1b[93m{positions.Aggregate(0, (acc, position) => (int)Math.Abs(position - positionToMoveTo) + acc)}\x1b[0m");
+        watch.Stop();
+        Console.WriteLine($"Part 1 Execution Time: {watch.ElapsedMilliseconds} ms");
+
+        watch.Start();
+        var bestMovement = int.MaxValue;
+
+        foreach(var tryPosition in Enumerable.Range(0, (int)positions.Max()))
+        {
+            bestMovement = Math.Min(bestMovement, positions.Aggregate(0, (acc, position) => {
+                                                        var n = (int)Math.Abs(position - tryPosition);
+                                                        return ((n * (n + 1)) / 2) + acc;
+                                                    }));
+        }
+
+        Console.WriteLine($"Part 2: \x1b[93m{bestMovement}\x1b[0m");
         watch.Stop();
         Console.WriteLine($"Part 2 Execution Time: {watch.ElapsedMilliseconds} ms");
     }
