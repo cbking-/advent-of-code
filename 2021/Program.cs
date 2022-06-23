45abd61909b26a44cd1ddc29cea76546131f46db
Corbin King <9155069+cbking-@users.noreply.github.com>
2022-06-22 23:23:32 -0400

    2021 - Day 3

--- 2021/Program.cs
+++ 2021/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 using System.Text;
 using static Core.Helpers;
 
@@ -11,8 +12,55 @@ dayToRun.Invoke(null, new object[] { data });
 public static class AdventOfCode
 {
     #region Objects
+    public class BingoBoard
+    {
+        public List<BingoPlace> Places { get; set; } = new List<BingoPlace>();
+
+        public int BoardWidth { get; set; } = 0;
+
+        public bool HasBingo()
+        {
+            //check rows
+            foreach (var row in Enumerable.Range(0, BoardWidth))
+            {
+                var MarkedCount = 0;
+
+                foreach (var column in Enumerable.Range(0, BoardWidth))
+                {
+                    MarkedCount += Places[column + row * BoardWidth].Marked ? 1 : 0;
+                }
+
+                if (MarkedCount == BoardWidth)
+                    return true;
+            }
+
+            //check columns
+            foreach (var column in Enumerable.Range(0, BoardWidth))
+            {
+                var MarkedCount = 0;
+
+                foreach (var row in Enumerable.Range(0, BoardWidth))
+                {
+                    MarkedCount += Places[column + row * BoardWidth].Marked ? 1 : 0;
+                }
+
+                if (MarkedCount == BoardWidth)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
+    [DebuggerDisplay("{Number} {Marked}")]
+    public class BingoPlace
+    {
+        public bool Marked { get; set; } = false;
+        public int Number { get; set; } = 0;
+    }
+
     #endregion
-    
+
     public static void Day1(string[] data)
     {
         var numbers = Array.ConvertAll(data, line => int.Parse(line));
@@ -46,28 +94,29 @@ public static class AdventOfCode
 
         //I don't think group by can be used here since
         // forward uses the current cumulative value of aim per iteration 
-        var calculation = data.Aggregate((0, 0 ,0),
-            (accumulator, line) => {
-            var split = line.Split(' ');
-
-            if (split[0] == "down")
+        var calculation = data.Aggregate((0, 0, 0),
+            (accumulator, line) =>
             {
-                accumulator.Item1 += int.Parse(split[1]);
-            }
+                var split = line.Split(' ');
 
-            if (split[0] == "up")
-            {
-                accumulator.Item1 -= int.Parse(split[1]);
-            }
+                if (split[0] == "down")
+                {
+                    accumulator.Item1 += int.Parse(split[1]);
+                }
 
-            if (split[0] == "forward")
-            {
-                accumulator.Item2 += int.Parse(split[1]);
-                accumulator.Item3 += accumulator.Item1 * int.Parse(split[1]);
-            }
+                if (split[0] == "up")
+                {
+                    accumulator.Item1 -= int.Parse(split[1]);
+                }
 
-            return accumulator;
-        });
+                if (split[0] == "forward")
+                {
+                    accumulator.Item2 += int.Parse(split[1]);
+                    accumulator.Item3 += accumulator.Item1 * int.Parse(split[1]);
+                }
+
+                return accumulator;
+            });
 
         Console.WriteLine($"Part 1: {calculation.Item2 * calculation.Item1}");
         Console.WriteLine($"Part 2: {calculation.Item2 * calculation.Item3}");
@@ -78,20 +127,20 @@ public static class AdventOfCode
         var gammaRate = new StringBuilder();
         var epsilonRate = new StringBuilder();
 
-        foreach(var index in Enumerable.Range(0, data[0].Length))
+        foreach (var index in Enumerable.Range(0, data[0].Length))
         {
             var groups = data.GroupBy(line => line.ElementAt(index));
             gammaRate.Append(groups.OrderBy(group => group.Count()).First().Key);
-            epsilonRate.Append(groups.OrderByDescending(group => group.Count()).First().Key);            
+            epsilonRate.Append(groups.OrderByDescending(group => group.Count()).First().Key);
         }
 
-        Console.WriteLine(Convert.ToInt32(gammaRate.ToString(), 2) * Convert.ToInt32(epsilonRate.ToString(), 2));
+        Console.WriteLine($"Part 1: {Convert.ToInt32(gammaRate.ToString(), 2) * Convert.ToInt32(epsilonRate.ToString(), 2)}");
 
         var diagnosticData = new List<String>(data);
         var oxyGenRate = new List<string>(data);
         var co2ScrubRate = new List<string>(data);
 
-        foreach(var index in Enumerable.Range(0, data[0].Length))
+        foreach (var index in Enumerable.Range(0, data[0].Length))
         {
             var groups = diagnosticData.GroupBy(line => line.ElementAt(index));
             oxyGenRate = oxyGenRate.Intersect(groups.OrderByDescending(group => group.Count())
@@ -104,17 +153,63 @@ public static class AdventOfCode
 
         diagnosticData = new List<String>(data);
 
-        foreach(var index in Enumerable.Range(0, data[0].Length))
+        foreach (var index in Enumerable.Range(0, data[0].Length))
         {
             var groups = diagnosticData.GroupBy(line => line.ElementAt(index));
             co2ScrubRate = co2ScrubRate.Intersect(groups.OrderBy(group => group.Count())
                                                         .ThenBy(group => group.Key)
                                                         .First()
                                                         .Select(group => group))
-                                                    .ToList(); 
+                                                    .ToList();
             diagnosticData = diagnosticData.Intersect(co2ScrubRate).ToList();
         }
 
-        Console.WriteLine(Convert.ToInt32(oxyGenRate.Single(), 2) * Convert.ToInt32(co2ScrubRate.Single(), 2));
+        Console.WriteLine($"Part 2: {Convert.ToInt32(oxyGenRate.Single(), 2) * Convert.ToInt32(co2ScrubRate.Single(), 2)}");
+    }
+
+    public static void Day4(string[] data)
+    {
+        var draws = Array.ConvertAll(data[0].Split(','), int.Parse);
+        var partOne = 0;
+        var partTwo = 0;
+        var boards = new List<BingoBoard>();
+        var boardWidth = 5;
+        var board = new BingoBoard() { BoardWidth = boardWidth };
+
+        foreach (var line in data.WithIndex().Skip(1))
+        {
+            var row = Array.ConvertAll(line.Item.Split(' ', StringSplitOptions.RemoveEmptyEntries), number => int.Parse(number));
+
+            foreach (var number in row.WithIndex())
+            {
+                var location = (number.Index % 5) + ((line.Index % 5) - 1) * boardWidth;
+                board.Places.Add(new BingoPlace { Number = number.Item });
+            }
+
+            if (line.Index % boardWidth == 0)
+            {
+                boards.Add(board);
+                board = new BingoBoard() { BoardWidth = boardWidth };
+            }
+        }
+
+        foreach (var draw in draws)
+        {
+            boards.ForEach(board => board.Places.Where(place => place.Number == draw).ToList().ForEach(place => place.Marked = true));
+
+            if (boards.Any(board => board.HasBingo()))
+            {
+                if(partOne == 0)
+                    partOne = boards.Where(board => board.HasBingo()).Single().Places.Where(place => !place.Marked).Sum(place => place.Number) * draw;
+
+                if(boards.Count() == 1)
+                    partTwo = boards.Single().Places.Where(place => !place.Marked).Sum(place => place.Number) * draw;
+
+                boards.RemoveAll(board => board.HasBingo());
+            }
+        }
+
+        Console.WriteLine($"Part 1: \x1b[93m{partOne}\x1b[0m");
+        Console.WriteLine($"Part 2: \x1b[93m{partTwo}\x1b[0m");
     }
 }
