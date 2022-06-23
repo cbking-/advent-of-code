0334756625fea40429b04ba98e82b9ca00626f0d
Corbin King <9155069+cbking-@users.noreply.github.com>
2022-06-22 23:23:25 -0400

    Day 7

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Collections.Concurrent;
+using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -12,6 +13,7 @@ await task;
 
 public static class AdventOfCode
 {
+    #region Helpers
     public static async Task<String> LoadDataAsync(string fileName)
     {
         using var file = new StreamReader(fileName);
@@ -50,6 +52,14 @@ public static class AdventOfCode
             return result;
         }
     }
+    public class Instruction
+    {
+        public string LeftOperand { get; set; } = string.Empty;
+        public string RightOperand { get; set; } = string.Empty;
+        public string Operation { get; set; } = string.Empty;
+        public string DestinationWire { get; set; } = string.Empty;
+    }
+    #endregion
 
     public static async Task Day1()
     {
@@ -335,5 +345,163 @@ public static class AdventOfCode
         }
 
         Console.WriteLine($"Part 2: {grid2.Sum(light => light)}");
+    }
+
+    public static async Task Day7()
+    {
+        //There's probably signficantly more efficient ways to do this one
+        // but was fun to figure out 
+        // 10/10
+        #region Setup
+        var data = await LoadDataAsync("input7");
+
+        var lines = data.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+        var instructions = lines.Select(line =>
+        {
+            var sources = line.Split(" -> ")[0];
+            var destination = line.Split(" -> ")[1];
+
+            var instruction = new Instruction();
+
+            if (sources.StartsWith("NOT"))
+            {
+                instruction.Operation = sources.Split(" ")[0];
+                instruction.RightOperand = sources.Split(" ")[1];
+            }
+            else if (sources.Split(" ").Length == 1)
+            {
+                instruction.RightOperand = sources;
+            }
+            else
+            {
+                instruction.LeftOperand = sources.Split(" ")[0];
+                instruction.Operation = sources.Split(" ")[1];
+                instruction.RightOperand = sources.Split(" ")[2];
+            }
+
+            instruction.DestinationWire = destination;
+
+            return instruction;
+        });
+
+        ConcurrentDictionary<string, int> map = new(instructions.Where(instruction => int.TryParse(instruction.RightOperand, out var temp) && instruction.LeftOperand == string.Empty)
+                            .ToDictionary(item => item.DestinationWire, item => int.Parse(item.RightOperand)));
+        #endregion
+
+        #region Part1
+        while (!map.ContainsKey("a"))
+        {
+            var instructionsToSolveNext = instructions.Where(instruction => (map.ContainsKey(instruction.LeftOperand) && map.ContainsKey(instruction.RightOperand)) //two wires
+                                                || (map.ContainsKey(instruction.RightOperand) && string.IsNullOrWhiteSpace(instruction.LeftOperand)) //NOT, wire -> wire
+                                                || (instruction.LeftOperand == "1" && map.ContainsKey(instruction.RightOperand)) // 1 AND wire
+                                                || (map.ContainsKey(instruction.LeftOperand) && int.TryParse(instruction.RightOperand, out var temp))); //wire SHIFT num
+
+            instructionsToSolveNext.AsParallel().ForAll(instruction =>
+            {
+                map.TryGetValue(instruction.LeftOperand, out var valueOne);
+                map.TryGetValue(instruction.RightOperand, out var valueTwo);
+
+                var finalValue = 0;
+
+                if (string.IsNullOrWhiteSpace(instruction.Operation))
+                {
+                    finalValue = valueTwo;
+                }
+                else if (instruction.Operation == "NOT")
+                {
+                    finalValue = ~valueTwo;
+                }
+                else if (instruction.Operation == "AND")
+                {
+                    if (instruction.LeftOperand == "1")
+                    {
+                        finalValue = 1 & valueTwo;
+                    }
+                    else
+                    {
+                        finalValue = valueOne & valueTwo;
+                    }
+                }
+                else if (instruction.Operation == "OR")
+                {
+                    finalValue = valueOne | valueTwo;
+                }
+                else if (instruction.Operation == "RSHIFT")
+                {
+                    finalValue = valueOne >> int.Parse(instruction.RightOperand);
+                }
+                else if (instruction.Operation == "LSHIFT")
+                {
+                    finalValue = valueOne << int.Parse(instruction.RightOperand);
+                }
+
+                map.TryAdd(instruction.DestinationWire, finalValue);
+            });
+        }
+
+        Console.WriteLine($"Part 1: {map["a"]}");
+        #endregion
+
+        #region Par2
+        var newSeed = map["a"];
+
+        map = new(instructions.Where(instruction => int.TryParse(instruction.RightOperand, out var temp) && instruction.LeftOperand == string.Empty)
+                            .ToDictionary(item => item.DestinationWire, item => int.Parse(item.RightOperand)));
+
+        map["b"] = newSeed;
+
+        while (!map.ContainsKey("a"))
+        {
+            var instructionsToSolveNext = instructions.Where(instruction => (map.ContainsKey(instruction.LeftOperand) && map.ContainsKey(instruction.RightOperand)) //two wires
+                                                || (map.ContainsKey(instruction.RightOperand) && string.IsNullOrWhiteSpace(instruction.LeftOperand)) //NOT, wire -> wire
+                                                || (instruction.LeftOperand == "1" && map.ContainsKey(instruction.RightOperand)) // 1 AND wire
+                                                || (map.ContainsKey(instruction.LeftOperand) && int.TryParse(instruction.RightOperand, out var temp))); //wire SHIFT num
+
+            instructionsToSolveNext.AsParallel().ForAll(instruction =>
+            {
+                map.TryGetValue(instruction.LeftOperand, out var valueOne);
+                map.TryGetValue(instruction.RightOperand, out var valueTwo);
+
+                var finalValue = 0;
+
+                if (string.IsNullOrWhiteSpace(instruction.Operation))
+                {
+                    finalValue = valueTwo;
+                }
+                else if (instruction.Operation == "NOT")
+                {
+                    finalValue = ~valueTwo;
+                }
+                else if (instruction.Operation == "AND")
+                {
+                    if (instruction.LeftOperand == "1")
+                    {
+                        finalValue = 1 & valueTwo;
+                    }
+                    else
+                    {
+                        finalValue = valueOne & valueTwo;
+                    }
+                }
+                else if (instruction.Operation == "OR")
+                {
+                    finalValue = valueOne | valueTwo;
+                }
+                else if (instruction.Operation == "RSHIFT")
+                {
+                    finalValue = valueOne >> int.Parse(instruction.RightOperand);
+                }
+                else if (instruction.Operation == "LSHIFT")
+                {
+                    finalValue = valueOne << int.Parse(instruction.RightOperand);
+                }
+
+                map.TryAdd(instruction.DestinationWire, finalValue);
+            });
+        }
+
+        Console.WriteLine($"Part 2: {map["a"]}");
+        #endregion
     }
 }
