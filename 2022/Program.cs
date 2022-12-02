3e2343f8dd3476f99a2fcca63f3bf23c8bab83cf
Corbin King <9155069+cbking-@users.noreply.github.com>
2022-12-02 17:34:08 -0500

    Day 2

--- 2022/Program.cs
+++ 2022/Program.cs
@@ -29,4 +29,64 @@ public class AdventOfCode
         Console.WriteLine(elves.Select(elf => elf.Sum()).Max());
         Console.WriteLine(elves.Select(elf => elf.Sum()).OrderByDescending(total => total).Take(3).Sum());
     }
+
+    public static void Day2(string[] data)
+    {
+        const string LOSE = "X";
+        const string DRAW = "Y";
+        const string WIN = "Z";
+
+        var states = new Dictionary<string, int>{
+            {"A X", 4},
+            {"A Y", 8},
+            {"A Z", 3},
+
+            {"B X", 1},
+            {"B Y", 5},
+            {"B Z", 9},
+
+            {"C X", 7},
+            {"C Y", 2},
+            {"C Z", 6}
+        };
+
+        var score = data.Aggregate(0, (acc, line) =>
+        {
+            var score = states[line];
+            return acc + score;
+        });
+
+        var scorePart2 = data.Aggregate(0, (acc, line) =>
+        {
+            var match = line.Split(' ');
+
+            var elf = match.First().ToCharArray().First().ToString();
+            var me = match.Last().ToCharArray().First().ToString();
+
+            var score = 0;
+            switch(me)
+            {
+                case WIN:
+                    score = states.Where(state => state.Key.StartsWith(elf))
+                        .Max(state => state.Value);
+                    break;
+
+                case LOSE:
+                    score = states.Where(state => state.Key.StartsWith(elf))
+                            .Min(state => state.Value);
+                    break;
+
+                case DRAW:
+                    score = states.Where(state => state.Key.StartsWith(elf))
+                            .OrderBy(state => state.Value).Skip(1).First().Value;
+                    break;
+            }
+
+            return acc + score;
+        });
+
+        Console.WriteLine(score);
+        Console.WriteLine(scorePart2);
+    }
+
 }
