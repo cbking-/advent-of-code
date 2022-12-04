6fada9248f327890ef8d02be7537973bb12f7943
Corbin King <9155069+cbking-@users.noreply.github.com>
2022-12-04 10:22:23 -0500

    Day 4

--- 2022/Program.cs
+++ 2022/Program.cs
@@ -127,4 +127,27 @@ public class AdventOfCode
         Console.WriteLine(prioritySum);
         Console.WriteLine(badgeSum);
     }
+
+    public static void Day4(string[] data)
+    {
+        var overlapping = data.Aggregate(new int[]{0, 0}, (acc, line) =>{
+            var elfOne = line.Split(',').First().Split('-').Select(Int32.Parse);
+            var elfTwo = line.Split(',').Last().Split('-').Select(Int32.Parse);
+
+            //completely overlapping
+            if ((elfOne.First() <= elfTwo.First() && elfOne.Last() >= elfTwo.Last())
+             || (elfTwo.First() <= elfOne.First() && elfTwo.Last() >= elfOne.Last()))
+                 acc[0] += 1;
+
+            //partially overlapping (is the end of one in the other's range)
+            if ((elfOne.Last() >= elfTwo.First() && elfOne.Last() <= elfTwo.Last())
+             || (elfTwo.Last() >= elfOne.First() && elfTwo.Last() <= elfOne.Last()))
+                 acc[1] += 1;
+
+            return acc;
+        });
+
+        Console.WriteLine(overlapping[0]);
+        Console.WriteLine(overlapping[1]);
+    }
 }
