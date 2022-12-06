cb9f84f5bd701bdadc3008e8f70eb469456a466b
Corbin King <9155069+cbking-@users.noreply.github.com>
2022-12-05 19:41:10 -0500

    Day 5

--- 2022/Program.cs
+++ 2022/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System.Collections;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
 using static Core.Helpers;
 
 var keepNewLines = new bool[25];
@@ -149,5 +152,94 @@ public class AdventOfCode
 
         Console.WriteLine(overlapping[0]);
         Console.WriteLine(overlapping[1]);
+    }
+
+    public static void Day5(string[] data)
+    {
+        //    [D]
+        //[N] [C]
+        //[Z] [M] [P]
+        // 1   2   3
+        //
+        //move 1 from 2 to 1
+        //move 3 from 1 to 3
+        //move 2 from 2 to 1
+        //move 1 from 1 to 2
+
+        var endInitialCreates = new Regex(@"^(\s*(\d+)\s*)+$");
+
+        var splitIndex = data.WithIndex()
+                             .Where(line => endInitialCreates.IsMatch(line.Item))
+                             .Select(line => line.Index)
+                             .Single();
+
+        var splitData = data.WithIndex()
+                            .GroupBy(line => line.Index > splitIndex)
+                            .Select(group => group.Select(line => line.Item));
+
+        var numberOfStacks = int.Parse(endInitialCreates.Matches(splitData.First().Last())
+                                              .Last().Groups[2].Value);
+
+        var stacks = new Stack[numberOfStacks].Select(stack => new Stack()).ToArray();
+        var betterStacks = new Stack[numberOfStacks].Select(stack => new Stack()).ToArray();
+
+        splitData.First().Reverse().Skip(1).ToList().ForEach(line =>
+        {
+            line.Skip(1)
+                .WithIndex()
+                .Where(crate => crate.Index % 4 == 0)
+                .ToList()
+                .ForEach(crate =>
+                {
+                    if(crate.Item != ' ')
+                    {
+                        stacks[crate.Index / 4].Push(crate.Item);
+                        betterStacks[crate.Index / 4].Push(crate.Item);
+                    }
+                });
+        });
+
+        splitData.Last().ToList().ForEach(move => {
+            var movePattern = new Regex(@"move (\d+) from (\d+) to (\d+)");
+            var moveData = movePattern.Matches(move);
+
+            var numberOfCrates = int.Parse(moveData.Single().Groups[1].Value);
+            var fromStack = int.Parse(moveData.Single().Groups[2].Value);
+            var toStack = int.Parse(moveData.Single().Groups[3].Value);
+
+            foreach(var index in Enumerable.Range(0, numberOfCrates))
+            {
+                stacks[toStack - 1].Push(stacks[fromStack - 1].Pop());
+            }
+
+            var interStack = new Stack();
+
+            foreach (var index in Enumerable.Range(0, numberOfCrates))
+            {
+                interStack.Push(betterStacks[fromStack - 1].Pop());
+            }
+
+            foreach (var index in Enumerable.Range(0, numberOfCrates))
+            {
+                betterStacks[toStack - 1].Push(interStack.Pop());
+            }
+        });
+
+        StringBuilder builder = new();
+
+        foreach(var stack in stacks){
+            builder.Append(stack.Pop());
+        }
+
+        Console.WriteLine(builder.ToString());
+
+        builder = new();
+
+        foreach (var stack in betterStacks)
+        {
+            builder.Append(stack.Pop());
+        }
+
+        Console.WriteLine(builder.ToString());
     }
 }
