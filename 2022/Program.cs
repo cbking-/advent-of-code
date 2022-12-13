af922d76a345d3c228263b4014db330675efc3d9
Corbin King <9155069+cbking-@users.noreply.github.com>
2022-12-12 22:36:27 -0500

    Day 9

--- 2022/Program.cs
+++ 2022/Program.cs
@@ -42,6 +42,130 @@ public class AdventOfCode
         public int Size = 0;
     }
 
+    private class Knot
+    {
+        public Knot()
+        {
+            //instead of figuring out how to deal with the head and tail being -1 and +1
+            // i'll just set the initial coordinates way out there so it's incredibly
+            // unlikely negatives even come in to consideration.
+            X = int.MaxValue / 2;
+            Y = int.MaxValue / 2;
+
+            Visited.Add($"{X},{Y}");
+        }
+
+        public int X;
+        public int Y;
+        public Knot? Child;
+        public HashSet<string> Visited = new();
+        public char Label = '0';
+        public Knot GetTail()
+        {
+            if (Child is not null)
+            {
+                return Child.GetTail();
+            }
+
+            return this;
+        }
+
+        public void Draw()
+        {
+            var grid = Enumerable.Repeat('.', 30 * 30).ToArray();
+            var knot = this;
+
+            while (knot is not null)
+            {
+                grid[knot.X + knot.Y * 30] = knot.Label;
+                knot = knot.Child;
+            }
+
+            Console.WriteLine(string.Join('\n', grid.Chunk(30).Reverse().Select(chunck => string.Join("", chunck))));
+            Console.WriteLine("=================");
+        }
+
+        public void Move(string direction, int steps)
+        {
+            foreach (var step in Enumerable.Range(1, steps))
+            {
+                switch (direction)
+                {
+                    case "U":
+                        Y += 1;
+                        break;
+
+                    case "R":
+                        X += 1;
+                        break;
+
+                    case "D":
+                        Y -= 1;
+                        break;
+
+                    case "L":
+                        X -= 1;
+                        break;
+                }
+
+                MoveChildren();
+
+                //Draw();
+            }
+        }
+
+        public void MoveChildren()
+        {
+            if (Child is null)
+                return;
+
+            if (Child.X - X == 2)
+            {
+                Child.X -= 1;
+
+                if (Child.Y - Y > 0)
+                    Child.Y -= 1;
+
+                if (Child.Y - Y < 0)
+                    Child.Y += 1;
+            }
+            else if (Child.X - X == -2)
+            {
+                Child.X += 1;
+
+                if (Child.Y - Y > 0)
+                    Child.Y -= 1;
+
+                if (Child.Y - Y < 0)
+                    Child.Y += 1;
+            }
+            else if (Child.Y - Y == 2)
+            {
+                Child.Y -= 1;
+
+                if (Child.X - X > 0)
+                    Child.X -= 1;
+
+                if (Child.X - X < 0)
+                    Child.X += 1;
+            }
+            else if (Child.Y - Y == -2)
+            {
+                Child.Y += 1;
+
+                if (Child.X - X > 0)
+                    Child.X -= 1;
+
+                if (Child.X - X < 0)
+                    Child.X += 1;
+            }
+
+
+            Child.Visited.Add($"{Child.X},{Child.Y}");
+            Child.MoveChildren();
+        }
+    }
+
     public static void Day1(string[] data)
     {
         var elves = data.Aggregate(new List<List<int>> { new List<int>() }, (acc, line) =>
@@ -420,11 +544,11 @@ public class AdventOfCode
                 var bTrees = 0;
 
                 //left edge
-                foreach(var x2 in Enumerable.Range(0, x).Reverse())
+                foreach (var x2 in Enumerable.Range(0, x).Reverse())
                 {
                     var otherIndex = x2 + y * width;
                     var otherTree = int.Parse(map[otherIndex].ToString());
-                    if(otherTree >= tree)
+                    if (otherTree >= tree)
                     {
                         if (x != 0 || x != width)
                             lTrees = x - x2;
@@ -434,7 +558,7 @@ public class AdventOfCode
                     }
                 }
 
-                if(lSeen)
+                if (lSeen)
                     lTrees = x;
 
                 //right edge
@@ -444,7 +568,7 @@ public class AdventOfCode
                     var otherTree = int.Parse(map[otherIndex].ToString());
                     if (otherTree >= tree)
                     {
-                        if(x != 0 || x != width)
+                        if (x != 0 || x != width)
                             rTrees = x2 - x;
 
                         rSeen = false;
@@ -452,7 +576,7 @@ public class AdventOfCode
                     }
                 }
 
-                if(rSeen)
+                if (rSeen)
                     rTrees = width - x - 1;
 
                 //top edge
@@ -470,7 +594,7 @@ public class AdventOfCode
                     }
                 }
 
-                if(tSeen)
+                if (tSeen)
                     tTrees = y;
 
                 //bottom edge
@@ -488,7 +612,7 @@ public class AdventOfCode
                     }
                 }
 
-                if(bSeen)
+                if (bSeen)
                     bTrees = height - y - 1;
 
                 if (lSeen || rSeen || tSeen || bSeen)
@@ -501,5 +625,51 @@ public class AdventOfCode
 
         Console.WriteLine($"Visible: {visCount}");
         Console.WriteLine($"Max Scenic Score: {maxScore}");
+    }
+
+    public static void Day9(string[] data)
+    {
+        var head = new Knot();
+        head.Label = 'H';
+        var width = 40;
+
+        Action Draw = () =>
+        {
+            var grid = Enumerable.Repeat('.', width * width).ToArray();
+            var knot = head;
+
+            while (knot is not null)
+            {
+                grid[knot.X + knot.Y * width] = knot.Label;
+                knot = knot.Child;
+            }
+
+            Console.WriteLine(string.Join('\n', grid.Chunk(width).Reverse().Select(chunck => string.Join("", chunck))));
+            Console.WriteLine("=================");
+        };
+
+
+        foreach (var index in Enumerable.Range(1, 9))
+        {
+            var child = new Knot();
+            child.Label = char.Parse(index.ToString());
+            head.GetTail().Child = child;
+        }
+
+        foreach (var line in data)
+        {
+            var instruction = line.Split(' ');
+            var direction = instruction[0];
+            var steps = int.Parse(instruction[1]);
+
+            head.Move(direction, steps);
+
+            //Draw();
+        }
+
+        //Console.WriteLine(string.Join('\n', head.GetTail().Visited));
+
+        Console.WriteLine(head.Child?.Visited.Count);
+        Console.WriteLine(head.GetTail().Visited.Count);
     }
 }
