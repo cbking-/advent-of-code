f5564786b3b8004eca4822ea655a4747a36de623
Corbin King <9155069+cbking-@users.noreply.github.com>
2022-12-12 23:24:33 -0500

    Day 10

--- 2022/Program.cs
+++ 2022/Program.cs
@@ -672,4 +672,58 @@ public class AdventOfCode
         Console.WriteLine(head.Child?.Visited.Count);
         Console.WriteLine(head.GetTail().Visited.Count);
     }
+
+    public static void Day10(string[] data)
+    {
+        int[] cycleChecks = {20, 60, 100, 140, 180, 220};
+        int[] signalStrenths = {0, 0, 0, 0, 0, 0};
+
+        var cycleNumber = 0;
+        var x = 1;
+        var pixelIndex = 0;
+
+        Action IncrementCycle = () =>{
+            if ((x + 1) == pixelIndex || (x - 1) == pixelIndex || x == pixelIndex)
+            {
+                Console.Write("\x2588");
+            }
+            else
+            {
+                Console.Write("\x2591");
+            }
+
+            pixelIndex += 1;
+            cycleNumber += 1;
+
+            if(cycleNumber % 40 == 0)
+            {
+                pixelIndex = 0;
+                Console.Write(Environment.NewLine);
+            }
+
+            if(cycleChecks.Contains(cycleNumber))
+            {
+                var signalIndex = Array.IndexOf(cycleChecks, cycleNumber);
+                signalStrenths[signalIndex] = cycleNumber * x;
+            }
+
+        };
+
+        foreach(var line in data)
+        {
+            var instruction = line.Split(' ');
+            var command = instruction[0];
+            int.TryParse(instruction.ElementAtOrDefault(1), out int argument);
+
+            IncrementCycle();
+
+            if(command == "addx")
+            {
+                IncrementCycle();
+                x += argument;
+            }
+        }
+
+        Console.WriteLine(signalStrenths.Sum());
+    }
 }
