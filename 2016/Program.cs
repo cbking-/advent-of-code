50417906a1c5d03bcfcfc7002959a5b0c6848bee
Corbin King <9155069+cbking-@users.noreply.github.com>
2023-08-28 23:18:41 -0400

    2016 - Day 1

---
+++ 2016/Program.cs
@@ -0,0 +1,113 @@
+﻿using static Core.Helpers;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+var data = await LoadDataAsync(args[0]);
+
+var adventType = typeof(AdventOfCode);
+var dayToRun = adventType.GetMethod(args[0], BindingFlags.Static | BindingFlags.Public) ?? throw new ArgumentException("Invalid day");
+dayToRun.Invoke(null, new object[] { data });
+
+public static class AdventOfCode
+{
+    public static void Day1(string[] data)
+    {
+        string[] input = data[0].Split(',', StringSplitOptions.TrimEntries);
+        int currentDirectionIndex = 0;
+        char[] directions = {'n', 'e', 's', 'w'};
+        Dictionary<char, int> accumulator = new()
+        {
+            {'n', 0},
+            {'e', 0},
+            {'s', 0},
+            {'w', 0}
+        };
+
+        foreach(string instruction in input)
+        {
+            if(instruction.StartsWith('L')){
+                currentDirectionIndex--;
+            }
+            else{
+                currentDirectionIndex++;
+            }
+
+            if(currentDirectionIndex == 4){
+                currentDirectionIndex = 0;
+            }
+            else if(currentDirectionIndex == -1){
+                currentDirectionIndex = 3;
+            }
+
+            accumulator[directions[currentDirectionIndex]] += int.Parse(instruction[1..]);
+        }
+
+        Console.WriteLine(Math.Abs(accumulator['n'] - accumulator['s']) + Math.Abs(accumulator['e'] - accumulator['w']));
+
+
+        HashSet<string> coords = new() {"0,0"};
+        int[] currentCoords = {0,0};
+
+        foreach(string instruction in input)
+        {
+            if (instruction.StartsWith('L'))
+            {
+                currentDirectionIndex--;
+            }
+            else
+            {
+                currentDirectionIndex++;
+            }
+
+            if (currentDirectionIndex == 4)
+            {
+                currentDirectionIndex = 0;
+            }
+            else if (currentDirectionIndex == -1)
+            {
+                currentDirectionIndex = 3;
+            }
+
+            bool breakLoop = false;
+
+            foreach(int _ in Enumerable.Range(0, int.Parse(instruction[1..]))){
+                switch (currentDirectionIndex)
+                {
+                    case 0:
+                        currentCoords[0]++;
+                        break;
+
+                    case 1:
+                        currentCoords[1]++;
+                        break;
+
+                    case 2:
+                        currentCoords[0]--;
+                        break;
+
+                    case 3:
+                        currentCoords[1]--;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if(coords.Contains(String.Join(",", currentCoords)))
+                {
+                    breakLoop = true;
+                    break;
+                }
+                else{
+                    coords.Add(String.Join(",", currentCoords));
+                }
+            }
+
+            if(breakLoop)
+                break;
+        }
+
+        Console.WriteLine(String.Join(",", currentCoords));
+    }
+}
