5ce88af93a6f46406b0866e1e6f68f756153beb1
Corbin King <9155069+cbking-@users.noreply.github.com>
2023-09-04 19:36:10 -0400

    Added running all days for a year. Added blank text files for 2021 and 2022

--- Program.cs
+++ Program.cs
@@ -9,19 +9,37 @@ global using System.Collections.Concurrent;
 global using System.Security.Cryptography;
 using System.Reflection;
 
-//ToDo: run all days in year
 //ToDo: run all days in all years
 
 //want to keep new lines for 2022 day 1 since elves are separated
 // by a new line
-var keepNewLines = false;
-if (args[0] == "2022" && args[1] == "Day1")
-    keepNewLines = true;
 
-Assembly.GetExecutingAssembly()
+if (args[1] == "All")
+{
+    List<Type> types = Assembly.GetExecutingAssembly()
         .GetTypes()
-        .Single(t => t.IsClass
-            && t.Namespace == $"Advent{args[0]}"
-            && t.Name == args[1])
-        .GetMethod("Run", BindingFlags.Static | BindingFlags.Public)
-       ?.Invoke(null, new object[] { await LoadDataAsync(args, keepNewLines) });
+        .Where(t => t.IsClass && t.Namespace == $"Advent{args[0]}" && t.Name.Contains("Day"))
+        .OrderBy(t => int.Parse(t.Name[3..]))
+        .ToList();
+
+    foreach (Type t in types)
+    {
+        Console.WriteLine();
+        Console.WriteLine(t.Name);
+        t.GetMethod("Run", BindingFlags.Static | BindingFlags.Public)
+        ?.Invoke(null, new object[] {
+            LoadDataAsync(new string[]{args[0], t.Name}
+                , args[0] == "2022" && t.Name == "Day1").Result
+        });
+    }
+}
+else
+{
+    Assembly.GetExecutingAssembly()
+            .GetTypes()
+            .Single(t => t.IsClass
+                && t.Namespace == $"Advent{args[0]}"
+                && t.Name == args[1])
+            .GetMethod("Run", BindingFlags.Static | BindingFlags.Public)
+           ?.Invoke(null, new object[] { await LoadDataAsync(args, args[0] == "2022" && args[1] == "Day1") });
+}
