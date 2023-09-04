cfa89e11f9f951f67ef393a31c555894287af425
Corbin King <9155069+cbking-@users.noreply.github.com>
2023-09-04 12:20:28 -0400

    2016 Day 5

---
+++ 2016/Day5.cs
@@ -0,0 +1,42 @@
+using System.Security.Cryptography;
+using System.Text;
+
+public static class Day5{
+    public static void Run(string[] data){
+        string input = data[0];
+        StringBuilder password = new();
+        StringBuilder passwordTwo = new("________");
+
+        foreach(int number in Enumerable.Range(0, int.MaxValue))
+        {
+            using MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{input}{number}");
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+            string hexValue = Convert.ToHexString(hashBytes);
+
+            if(hexValue[..5] == "00000")
+            {
+                if (password.Length < 8)
+                {
+                    password.Append(hexValue[5]);
+                }
+
+
+                if (int.TryParse(char.ToString(hexValue[5]), out int result)
+                    && result < 8
+                    && passwordTwo[result] == '_')
+                {
+                    passwordTwo[result] = hexValue[6];
+                }
+            }
+
+            if(password.Length == 8 && !passwordTwo.ToString().Contains('_'))
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("Door one: " + password);
+        Console.WriteLine("Door two: " + passwordTwo);
+    }
+}
